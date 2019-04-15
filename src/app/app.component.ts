896cd83578274fe27f9ac765e81fb1f886a41a6b
edirect edirect <bouabid.aymen@gmail.com>
2019-04-15 22:27:26 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Post } from './post';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'blog-angular';
+  posts: Array<Post> = [
+    {
+      title: 'Premier post',
+      content: 'Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression 11111',
+      loveIts: 3,
+      created_at: new Date()
+    },
+    {
+      title: 'Deuxième post',
+      content: 'Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression 22222',
+      loveIts: -1,
+      created_at: new Date()
+    },
+    {
+      title: 'Troisième post',
+      content: 'Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression 33333',
+      loveIts: 0,
+      created_at: new Date()
+    }
+  ];
+
 }
